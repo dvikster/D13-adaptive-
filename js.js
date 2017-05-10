b1090ba085662a9007df56a4ea6f28356dc75733
dvikster <dvikster@gmail.com>
2017-05-10 12:24:57 +0300

    added files

--- js.js
+++ js.js
@@ -1,17 +1,18 @@
 var wrap=document.getElementsByClassName('valign_wrapp');
 var contentBox=document.getElementsByClassName('valign');
-var mTop=[];
 
 function verH() {
     for (var i=0; i< wrap.length; i++) {
-        contentBox[i].style.marginTop = '0';
-        mTop[i] = (wrap[i].offsetHeight - contentBox[i].offsetHeight)/2;
-        contentBox[i].style.marginTop = mTop[i] + 'px';
+        contentBox[i].style.marginTop = (wrap[i].offsetHeight - contentBox[i].offsetHeight)/2 + 'px';
     }
 }
 verH();
 
-window.onresize = function () {
-    verH();
-}
+// сокращенная запись
+window.onresize = verH;
 
+
+// развернутая запись
+// window.onresize = function () {
+//     verH();
+// }
