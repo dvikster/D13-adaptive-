101706e494cde62ace519fc2bd93209e5e878149
dvikster <dvikster@gmail.com>
2017-05-10 12:16:01 +0300

    added files

--- js.js
+++ js.js
@@ -1,29 +1,17 @@
 var wrap=document.getElementsByClassName('valign_wrapp');
 var contentBox=document.getElementsByClassName('valign');
-
-
-var hWrapp=[];
-var hContentBox=[];
 var mTop=[];
-
 
 function verH() {
     for (var i=0; i< wrap.length; i++) {
-        hWrapp[i] = wrap[i].offsetHeight;
-        console.log(hWrapp[i]);
-        hContentBox[i] = contentBox[i].offsetHeight;
-        console.log(hContentBox[i]);
-        mTop[i] = (hWrapp[i] - hContentBox[i])/2;
-        console.log(mTop[i]);
+        contentBox[i].style.marginTop = '0';
+        mTop[i] = (wrap[i].offsetHeight - contentBox[i].offsetHeight)/2;
         contentBox[i].style.marginTop = mTop[i] + 'px';
     }
 }
 verH();
 
 window.onresize = function () {
-    for (var i=0; i< wrap.length; i++) {
-        contentBox[i].style.marginTop = '0';
-    }
     verH();
 }
 
